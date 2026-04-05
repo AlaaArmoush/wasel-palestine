eef9829d5d2e0e4e1e97b9743b675458e93c292b
Alaa Armoush <armoush2004@gmail.com>
2026-04-06 01:57:27 +0300

    base applications and build test

---
+++ app/api/v1/router.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from app.api.v1.endpoints import (
+    auth,
+    users,
+    checkpoints,
+    incidents,
+    reports,
+    routes,
+    alerts,
+)
+
+api_router = APIRouter()
+
+api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
+api_router.include_router(
+    checkpoints.router, prefix="/checkpoints", tags=["Checkpoints"]
+)
+api_router.include_router(incidents.router, prefix="/incidents", tags=["Incidents"])
+api_router.include_router(reports.router, prefix="/reports", tags=["Reports"])
+api_router.include_router(routes.router, prefix="/routes", tags=["Route Estimation"])
+api_router.include_router(alerts.router, prefix="/alerts", tags=["Alerts"])
