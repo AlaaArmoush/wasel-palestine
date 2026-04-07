f2eebd46fad9f14bd7f77af4f641d2eb63a52988
Alaa Armoush <armoush2004@gmail.com>
2026-04-08 02:39:33 +0300

    get and update profile endpoints

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,3 +1,24 @@
 from fastapi import APIRouter
 
+from app.core.dependencies import DB, CurrentUser
+from app.schemas.user import UserOut, UserUpdate
+from app.services import users as service
+from app.utils.responses import success_response
+
 router = APIRouter()
+
+
+@router.get("/me")
+def get_my_profile(current_user: CurrentUser):
+    user = service.get_own_profile(current_user)
+    return success_response(
+        data=UserOut.model_validate(user), message="Profile Retrieved"
+    )
+
+
+@router.patch("/me")
+def update_own_profile(db: DB, current_user: CurrentUser, payload: UserUpdate):
+    updated_user = service.update_own_profile(db, current_user, payload)
+    return success_response(
+        data=UserOut.model_validate(updated_user), message="Profile Updated"
+    )
---
+++ app/services/users.py
@@ -0,0 +1,35 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+from app.schemas.user import UserUpdate
+
+
+def get_own_profile(user: User) -> User:
+    return user
+
+
+def update_own_profile(db: Session, user: User, payload: UserUpdate) -> User:
+    # convert model instance into a dictionary
+    update_data = payload.model_dump(exclude_unset=True)
+
+    if not update_data:
+        return user
+
+    if "username" in update_data:
+        exists = (
+            db.query(User)
+            .filter(User.username == update_data["username"], User.id != user.id)
+            .first()
+        )
+        if exists:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Username already taken"
+            )
+
+    for field, value in update_data.items():
+        setattr(user, field, value)
+
+    db.commit()
+    db.refresh(user)
+    return user
