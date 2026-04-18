32feecdcafff9f601c1cd1f461648e9df68bf4ff
Alaa Armoush <armoush2004@gmail.com>
2026-04-18 03:02:24 +0300

    PATCH endpoint: update user's role by id

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.core.dependencies import DB, CurrentUser, AdminOnly
-from app.schemas.user import UserOut, UserUpdate
+from app.schemas.user import UserOut, UserRoleUpdate, UserUpdate
 from app.services import users as service
 from app.utils.responses import success_response
 from uuid import UUID
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 @router.get("/me")
-def get_my_profile(current_user: CurrentUser):
+def get_own_profile(current_user: CurrentUser):
     user = service.get_own_profile(current_user)
     return success_response(
         data=UserOut.model_validate(user), message="Profile Retrieved"
@@ -26,6 +26,15 @@ def update_own_profile(db: DB, current_user: CurrentUser, payload: UserUpdate):
 
 
 @router.get("/{user_id}", dependencies=[AdminOnly])
-def get_user(user_id: UUID, db: DB):
+def get_user_by_id(user_id: UUID, db: DB):
     user = service.get_user_by_id(db, user_id)
     return success_response(data=UserOut.model_validate(user), message="User Retrieved")
+
+
+@router.patch("/{user_id}/role", dependencies=[AdminOnly])
+def update_user_role(user_id: UUID, payload: UserRoleUpdate, db: DB):
+    user = service.update_user_role(db, user_id, payload)
+    return success_response(
+        data=UserOut.model_validate(user),
+        message=f"User role updated to {user.role.value}",
+    )
